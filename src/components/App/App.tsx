893869a4a2a880c76760b8798174c71a3a202ac6
AbrahamDN <abe.ndinga@gmail.com>
2022-10-25 08:09:21 +0100

    feat: invent folder structure - moved App to /components & created Layout file

---
+++ src/components/App/App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1 className="h1">Heading 1</h1>
+        <h2 className="h2">Heading 2</h2>
+        <h3 className="h3">Heading 3</h3>
+        <h4 className="h4">Heading 4</h4>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
+          suscipit voluptates saepe cumque modi!
+        </p>
+
+        <a href="/" className="link">
+          Link
+        </a>
+
+        <br />
+        <div className="demo">
+          <div className="box primary">Primary</div>
+          <div className="box secondary">Secondary</div>
+          <div className="box accent">Accent</div>
+          <div className="box neutral">Neutral</div>
+          <div className="box base-200">Base</div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

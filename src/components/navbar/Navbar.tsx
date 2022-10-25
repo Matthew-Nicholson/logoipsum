6817e24f3ffea0f69e1aae10860b7f5d633ef609
Matt Nicholson <matthewn1991@gmail.com>
2022-10-24 21:31:31 -0400

    feat(navbar): init navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import logo from '../../assets/nav_logo.svg'
+
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+      <img src={logo} alt="Logoipsum" />
+    </nav>
+  )
+}
